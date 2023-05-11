350d78d4a5f606b907503176cc934e2237a39d9c
unknown <binayrijal22@gmail.com>
2023-05-11 22:09:47 +0545

    about get and post

--- newone/newone/urls.py
+++ newone/newone/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('show.urls')),
+    path('submitted/',include('show.urls')),
     path('admin/', admin.site.urls),
     
 ]
--- newone/show/views.py
+++ newone/show/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+
      return render(request,'index.html')
+def submitted(request):
+     name=request.POST['name']
+     address=request.POST['address']
+     email=request.POST['email']
+     return render(request,'show.html',{"name":name},{"address":address},{"email":email})
