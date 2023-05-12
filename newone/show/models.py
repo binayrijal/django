ee51ddc74f2addcfc91ff7f9185c3ef4418159df
unknown <binayrijal22@gmail.com>
2023-05-12 14:46:23 +0545

    object pass in view

--- newone/show/models.py
+++ newone/show/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class show:
+    names:str
+    addresss:str
+    emails:str
+
--- newone/show/views.py
+++ newone/show/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import show
 
 # Create your views here.
 def home(request):
@@ -9,4 +10,9 @@ def submitted(request):
      name=request.POST['name']
      address=request.POST['address']
      email=request.POST['email']
-     return render(request,'show.html',{"name":name},{"address":address},{"email":email})
+     c=show()
+     c.names="binay"
+     c.addresss="hasanapur"
+     c.emails="bin@gmail.com"
+
+     return render(request,'show.html',{"name":name,"address":address,"email":email,"c":c})
